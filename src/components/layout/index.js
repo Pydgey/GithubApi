ad0feab76df5a85cfbdce1be4c83440843792e97
Pydgey <97felipe3d@gmail.com>
2022-01-04 22:13:53 -0300

    Profile Mocked

---
+++ src/components/layout/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Layout({children}) {
+    return (
+        <section>
+            <header>header</header>
+            {children}
+        </section>
+    );
+}
+
+export default Layout;
---
+++ src/components/profile/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import * as S from "./styled";
+
+const Profile = () => {
+    return (
+        <S.Wrapper>
+            <S.WrapperImg src="https://avatars.githubusercontent.com/u/79947677?v=4" alt="User Avatar"/>
+            <S.WrapperInfoUser>
+                <div>
+                    <h1>Felipe Reis</h1>
+                    <S.WrapperUsername>
+                        <h3>Username:</h3>
+                        <span>Pydgey</span>
+                    </S.WrapperUsername>
+                </div>
+                <S.WrapperStatusCount>
+                    <div>
+                        <h4>Followers:</h4>
+                        <span>0</span>
+                    </div>
+
+                    <div>
+                        <h4>Starreds:</h4>
+                        <span>1</span>
+                    </div>
+
+                    <div>
+                        <h4>Followings:</h4>
+                        <span>0</span>
+                    </div>
+                </S.WrapperStatusCount>
+            </S.WrapperInfoUser>
+        </S.Wrapper>
+    );
+}
+
+export default Profile;
